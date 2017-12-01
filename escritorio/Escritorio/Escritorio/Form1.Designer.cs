6577a4e004c534708d4056365867162321a11f7c
jcpesantesreyna <jcpesantesreyna@gmail.com>
2017-11-30 23:40:16 -0500

    interfaz escreitorio

--- escritorio/Escritorio/Escritorio/Form1.Designer.cs
+++ escritorio/Escritorio/Escritorio/Form1.Designer.cs
@@ -30,99 +30,177 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.lblLunes = new System.Windows.Forms.Label();
+            this.lblMartes = new System.Windows.Forms.Label();
+            this.lblMiercoles = new System.Windows.Forms.Label();
+            this.lblJueves = new System.Windows.Forms.Label();
+            this.lblViernes = new System.Windows.Forms.Label();
             this.helpProvider1 = new System.Windows.Forms.HelpProvider();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblSabado = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Location = new System.Drawing.Point(23, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            this.label1.Text = "Turno";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(173, 42);
+            this.label2.Location = new System.Drawing.Point(113, 42);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 8;
             this.label2.Text = "label2";
             // 
-            // label3
+            // lblLunes
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(232, 42);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "label3";
+            this.lblLunes.AutoSize = true;
+            this.lblLunes.Location = new System.Drawing.Point(244, 42);
+            this.lblLunes.Name = "lblLunes";
+            this.lblLunes.Size = new System.Drawing.Size(19, 13);
+            this.lblLunes.TabIndex = 9;
+            this.lblLunes.Text = "00";
             // 
-            // label4
+            // lblMartes
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(273, 42);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "label4";
+            this.lblMartes.AutoSize = true;
+            this.lblMartes.Location = new System.Drawing.Point(273, 42);
+            this.lblMartes.Name = "lblMartes";
+            this.lblMartes.Size = new System.Drawing.Size(19, 13);
+            this.lblMartes.TabIndex = 10;
+            this.lblMartes.Text = "00";
             // 
-            // label5
+            // lblMiercoles
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(386, 42);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "label5";
+            this.lblMiercoles.AutoSize = true;
+            this.lblMiercoles.Location = new System.Drawing.Point(304, 42);
+            this.lblMiercoles.Name = "lblMiercoles";
+            this.lblMiercoles.Size = new System.Drawing.Size(19, 13);
+            this.lblMiercoles.TabIndex = 11;
+            this.lblMiercoles.Text = "00";
             // 
-            // label6
+            // lblJueves
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(464, 42);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "label6";
+            this.lblJueves.AutoSize = true;
+            this.lblJueves.Location = new System.Drawing.Point(337, 42);
+            this.lblJueves.Name = "lblJueves";
+            this.lblJueves.Size = new System.Drawing.Size(19, 13);
+            this.lblJueves.TabIndex = 12;
+            this.lblJueves.Text = "00";
             // 
-            // label7
+            // lblViernes
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(535, 42);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "label7";
+            this.lblViernes.AutoSize = true;
+            this.lblViernes.Location = new System.Drawing.Point(371, 42);
+            this.lblViernes.Name = "lblViernes";
+            this.lblViernes.Size = new System.Drawing.Size(19, 13);
+            this.lblViernes.TabIndex = 13;
+            this.lblViernes.Text = "00";
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(40, 64);
+            this.monthCalendar1.Location = new System.Drawing.Point(235, 64);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Mañana",
+            "Tarde"});
+            this.comboBox1.Location = new System.Drawing.Point(26, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(39, 104);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Servicio";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(26, 131);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(135, 21);
+            this.comboBox2.TabIndex = 18;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(26, 192);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(197, 21);
+            this.comboBox3.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 159);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Medico";
+            // 
+            // lblSabado
+            // 
+            this.lblSabado.AutoSize = true;
+            this.lblSabado.Location = new System.Drawing.Point(402, 42);
+            this.lblSabado.Name = "lblSabado";
+            this.lblSabado.Size = new System.Drawing.Size(19, 13);
+            this.lblSabado.TabIndex = 21;
+            this.lblSabado.Text = "00";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(29, 250);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(433, 166);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(762, 436);
+            this.ClientSize = new System.Drawing.Size(498, 453);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblSabado);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.monthCalendar1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblViernes);
+            this.Controls.Add(this.lblJueves);
+            this.Controls.Add(this.lblMiercoles);
+            this.Controls.Add(this.lblMartes);
+            this.Controls.Add(this.lblLunes);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,13 +210,20 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblLunes;
+        private System.Windows.Forms.Label lblMartes;
+        private System.Windows.Forms.Label lblMiercoles;
+        private System.Windows.Forms.Label lblJueves;
+        private System.Windows.Forms.Label lblViernes;
         private System.Windows.Forms.HelpProvider helpProvider1;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblSabado;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
